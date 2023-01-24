8df2cfc054f650a9cf2f0ab559c60ec5c451639d
Rafli Ramadhan <rafliramdhn@gmail.com>
2023-01-24 23:58:33 +0700

    add about us and footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -53,19 +53,60 @@ function Home() {
         {/* End Provide Section */}
 
         {/* Choose Section */}
-        <section id="why-us" className='px-16 py-10'>
+        <section id="why-us" className='px-16 py-20'>
             <div className='uppercase text-gold font-bold text-2xl text-center'>Why Choose Syra?</div>
-            <div className='grid grid-cols-3 gap-4'>
+            <div className='grid grid-cols-3 gap-4 my-16'>
                 <div className='flex justify-center'>
-                    <div>
-                        <div>Logo</div>
-                        <div>Bla</div>
-                        <div>asd</div>
+                    <div className='text-center space-y-3'>
+                        <div className='font-bold text-xl'>Innovation</div>
+                        <div>We keep working on the ideas of discovering, creating and developing products and services with high appeal in the marketplace / We also try our best to make Syra as the customers first choice.</div>
+                    </div>
+                </div>
+                <div className='flex justify-center'>
+                    <div className='text-center space-y-3'>
+                        <div className='font-bold text-xl'>Quality</div>
+                        <div>We define quality as the action of providing products and services that satisfy customers, good work environments, maximize the delivery of financial result as investors are expected.</div>
+                    </div>
+                </div>
+                <div className='flex justify-center'>
+                    <div className='text-center space-y-3'>
+                        <div className='font-bold text-xl'>Integrity</div>
+                        <div>Integrity is one of the most valuable assets we value the most. We see integrity as an honest act by telling the truth and sticking to what has been agreed</div>
                     </div>
                 </div>
             </div>
         </section>
         {/* End Choose Section */}
+
+        {/* Footer Section */}
+        <footer className='bg-black text-white'>
+            <div className='flex justify-center'>
+                <div className='w-5/6 py-10'>
+                    <div className='flex justify-between'>
+                        <div className='w-4/12 space-y-3'>
+                            <div className='font-bold text-xl'>Syra International</div>
+                            <div className='text-sm text-justify'>Syra International is a distributor company that specializes in a wide range of products and services, including Agriculture, Teak Root Furniture, Iron, Biodiesel, and Coal, as well as freight forwarding services such as Inland, Air, and Sea Freight Service. We have a large sales capacity and export their products to various countries. We are dedicated to providing the best service and fulfilling the needs of clients.</div>
+                        </div>
+                        <div className='w-2/12 space-y-3'>
+                            <div className='font-bold text-xl'>Contact Us</div>
+                            <div className='text-sm'>Jl. Raya Cikarang Barat No. 1, Cikarang Barat, Bekasi, Jawa Barat 17530</div>
+                            <div className='text-sm'>+62 21 8999 9999</div>
+                            <div className='text-sm'>
+                            </div>
+                        </div>
+                        <div className='w-2/12 space-y-3'>
+                            <div className='font-bold text-xl'>Contact Us</div>
+                            <div className='text-sm'>Jl. Raya Cikarang Barat No. 1, Cikarang Barat, Bekasi, Jawa Barat 17530</div>
+                            <div className='text-sm'>+62 21 8999 9999</div>
+                            <div className='text-sm'>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        {/* End Footer Section */}
     </div>
   )
 }
